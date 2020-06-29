0fdcf81e32d42413024435dd9c0a27fd73ab5785
LILNT <64647707+L1LNT@users.noreply.github.com>
2020-06-29 22:19:32 +0100

    Add files via upload

---
+++ ValidarData.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class ValidarData {
+public static void main(String args[]){
+Scanner entrada = new Scanner(System.in);
+
+   byte dia;
+   byte mes;
+   int ano;
+   String data;
+   
+   System.out.print("Digite o dia: ");
+   dia = entrada.nextByte();
+   System.out.print("Digite o mes: ");
+   mes = entrada.nextByte();
+   System.out.print("Digite o ano: ");
+   ano = entrada.nextInt();
+   
+   if(dia>0 && dia<32 && mes>0 && mes<13 && ano>0 && ((mes==1 || mes==3 || mes==5 || mes==7 || mes==8 || mes==10 || mes==12) || ((mes==4 || mes==6 || mes==9 || mes==11) && dia<=30) || (mes==2 &&(dia<=29 && ano%4==0 && (ano%100!=0 || ano%400==0))|| dia<=28))){
+       data="DATA VALIDA";
+   }else{
+       data="DATA INVALIDA";
+   }
+   
+   System.out.println(+dia+"/"+mes+"/"+ano+" "+data);
+}
+}
